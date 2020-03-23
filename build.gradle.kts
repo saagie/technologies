84e0ec53064257af4931df1e0078736588bab149
Pierre Leresteux <pierre.leresteux@gmail.com>
2020-03-23 11:32:15 +0100

    Upgrade saagie-technologies-plugin version to fix build problems

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ buildscript {
         mavenLocal()
     }
     dependencies {
-        classpath("com.saagie:technologiesplugin:1.1.2")
+        classpath("com.saagie:technologiesplugin:1.1.3")
     }
 }
 apply<SaagieTechnologiesPackageGradlePlugin>()
