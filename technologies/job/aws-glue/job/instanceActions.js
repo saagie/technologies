182fd05188b2155f22cf250e460349429a787ac2
Youen Chéné <youen@saagie.com>
2020-03-27 07:26:40 +0100

    Init aws-glue

---
+++ technologies/job/aws-glue/job/instanceActions.js
@@ -0,0 +1,90 @@
+const axios = require('axios');
+const { Response, JobStatus } = require('@saagie/sdk');
+const AWS = require('aws-sdk');
+
+/**
+ * Logic to start the external job instance.
+ * @param {Object} params
+ * @param {Object} params.job - Contains job data including featuresValues.
+ * @param {Object} params.instance - Contains instance data.
+ */
+exports.start = async ({ job, instance }) => {
+  try {
+    console.log('START INSTANCE:', instance);
+    AWS.config.update({credentials: { accessKeyId : job.featuresValues.endpoint.aws_access_key_id, secretAccessKey:  job.featuresValues.endpoint.aws_secret_access_key}});
+    AWS.config.update({region: job.featuresValues.endpoint.region});
+
+    var glue = new AWS.Glue();
+
+    const data = await glue.startJobRun({ JobName: job.featuresValues.job.id }).promise();
+
+    return Response.success({ glueJobId: data.JobRunId });
+  } catch (error) {
+    return Response.error('Fail to start job', { error });
+  }
+};
+
+/**
+ * Logic to stop the external job instance.
+ * @param {Object} params
+ * @param {Object} params.job - Contains job data including featuresValues.
+ * @param {Object} params.instance - Contains instance data including the payload returned in the start function.
+ */
+exports.stop = async ({ job, instance }) => {
+  try {
+    console.log('STOP INSTANCE:', instance);
+    AWS.config.update({credentials: { accessKeyId : job.featuresValues.endpoint.aws_access_key_id, secretAccessKey:  job.featuresValues.endpoint.aws_secret_access_key}});
+    AWS.config.update({region: job.featuresValues.endpoint.region});
+
+    var glue = new AWS.Glue();
+
+    const data = await glue.batchStopJobRun({ JobName: job.featuresValues.job.id, JobRunIds: [ instance.payload.glueJobId ] }).promise();
+
+    console.log('STOPPED', data);
+
+    return Response.success();
+  } catch (error) {
+    return Response.error('Fail to stop job', { error });
+  }
+};
+
+/**
+ * Logic to retrieve the external job instance status.
+ * @param {Object} params
+ * @param {Object} params.job - Contains job data including featuresValues.
+ * @param {Object} params.instance - Contains instance data including the payload returned in the start function.
+ */
+exports.getStatus = async ({ job, instance }) => {
+  try {
+    AWS.config.update({credentials: { accessKeyId : job.featuresValues.endpoint.aws_access_key_id, secretAccessKey:  job.featuresValues.endpoint.aws_secret_access_key}});
+    AWS.config.update({region: job.featuresValues.endpoint.region});
+
+    var glue = new AWS.Glue();
+
+    const data = await glue.getJobRun({ JobName: job.featuresValues.job.id, RunId: instance.payload.glueJobId }).promise();
+
+    console.log('STATUS', data.JobRun.JobRunState);
+
+
+    switch (data.JobRun.JobRunState) {
+      case 'RUNNING':
+        return Response.success(JobStatus.RUNNING);
+      case 'STOPPED':
+        return Response.success(JobStatus.KILLED);
+      case 'SUCCEEDED':
+        return Response.success(JobStatus.SUCCEEDED);
+      case 'STARTING':
+        return Response.success(JobStatus.REQUESTED);
+      case 'FAILED':
+        return Response.success(JobStatus.FAILED);
+      case 'STOPPING':
+        return Response.success(JobStatus.KILLING);
+      case 'TIMEOUT':
+        return Response.success(JobStatus.FAILED);
+      default:
+        return Response.success(JobStatus.AWAITING);
+    }
+  } catch (error) {
+    return Response.error(`Failed to get status for instance ${instance}`, { error });
+  }
+};
---
+++ technologies/job/aws-glue/job/jobForm.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const { Response } = require('@saagie/sdk');
+const AWS = require('aws-sdk');
+
+/**
+ * Example of function to retrieve select options from an external endpoint.
+ * @param {Object} entity - Contains entity data including featuresValues.
+ * @param {Object} entity.featuresValues - Contains all the values from the entity features declared in the context.yaml
+ */
+exports.getJobs = async ({ featuresValues }) => {
+  try {
+    AWS.config.update({credentials: { accessKeyId : featuresValues.endpoint.aws_access_key_id, secretAccessKey:  featuresValues.endpoint.aws_secret_access_key}});
+    AWS.config.update({region: featuresValues.endpoint.region});
+
+    var glue = new AWS.Glue();
+
+    const data = await glue.getJobs().promise();
+    if (!data || !data.Jobs || !data.Jobs.length) {
+      return Response.empty('No jobs availables');
+    }
+    
+    return Response.success(
+      data.Jobs.map(({ Name }) => ({
+        id: Name,
+        label: Name,
+      })),
+    );
+  } catch (error) {
+    return Response.error("Can't retrieve jobs", { error });
+  }
+};
