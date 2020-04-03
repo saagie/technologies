45ff606955b11c8eec468573a2129495c8232524
Youen Chéné <youen@saagie.com>
2020-04-03 08:14:11 +0200

    [nifi] add logs + upgrade sdk to 0.6.0

--- technologies/job/nifi/1.9.2/instanceActions.js
+++ technologies/job/nifi/1.9.2/instanceActions.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const { Response, JobStatus } = require('@saagie/sdk');
+const { Response, JobStatus, Log } = require('@saagie/sdk');
 
 /**
  * Logic to start the external job instance.
@@ -55,6 +55,7 @@ exports.stop = async ({ job, instance }) => {
  */
 exports.getStatus = async ({ job, instance }) => {
   try {
+    console.log('GET LOG INSTANCE:', instance);
     const { data } = await axios.get(
       `${job.featuresValues.endpoint.url}/nifi-api/flow/process-groups/${job.featuresValues.processgroups.id}/status`,
     );
@@ -73,21 +74,33 @@ exports.getStatus = async ({ job, instance }) => {
 };
 
 /**
- * Logic to retrieve the external job instance status.
+ * Logic to retrieve the external job instance logs.
  * @param {Object} params
  * @param {Object} params.job - Contains job data including featuresValues.
  * @param {Object} params.instance - Contains instance data including the payload returned in the start function.
  */
 exports.getLogs = async ({ job, instance }) => {
-
   try {
+    console.log('GET LOG INSTANCE:', instance);
     const { data: logs } = await axios.get(
-      `${job.featuresValues.endpoint.url}/nifi-api/flow/bulletin-board?after=299213`,
+      `${job.featuresValues.endpoint.url}/nifi-api/flow/bulletin-board?groupId${job.featuresValues.processgroups.id}`,
     );
-
-    return Response.success(logs.bulletinBoard.bulletins);
-
+    const output = logs.bulletinBoard.bulletins
+      .map(item => {
+        const d=new Date();
+        const isodate = (new Date(d.getUTCMonth()+ ' ' + d.getUTCDate() + ', ' + d.getUTCFullYear() + ' '+ item.bulletin.timestamp)).toISOString();
+        if (item.bulletin.level=='ERROR') {
+         return Log(item.bulletin.message,'stderr',isodate);
+        } else {
+          return Log(item.bulletin.message,'stdout',isodate);
+        }
+      });
+    
+    console.log(output);
+    
+    return Response.success(output);
   } catch (error) {
+    console.log(error);
     return Response.error(`Fail to get logs of job " ${job.featuresValues.dataset.id}`, { error });
   }
 };
