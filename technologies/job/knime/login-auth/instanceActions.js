d5ce521b8ce7d84079df99abb7a47fb51e4bb5b7
Nicolas <nicolas.torion@bearstudio.fr>
2020-04-27 11:45:25 +0200

    [knime] Add job logs

--- technologies/job/knime/login-auth/instanceActions.js
+++ technologies/job/knime/login-auth/instanceActions.js
@@ -1,5 +1,47 @@
 const axios = require('axios');
-const { Response, JobStatus, Log } = require('@saagie/sdk');
+const { Response, JobStatus, Log, Stream } = require('@saagie/sdk');
+const fs = require('fs');
+const extract = require('extract-zip');
+const rimraf = require('rimraf');
+const dayjs =  require('dayjs');
+
+const getLogsForAJob = (logsLines, lineIndex, jobIdString) => {
+  let currentLineIndex = lineIndex;
+  let logs = [];
+
+  if (logsLines && logsLines.length > currentLineIndex) {
+    // We are detecting if the current line is the first of a log block (start by a date) for the selected job
+    const maybeDate = logsLines[currentLineIndex].substring(0, 19);
+    let isAValidDate = dayjs(maybeDate).isValid();
+
+    if (isAValidDate && logsLines[currentLineIndex].includes(jobIdString)) {
+      // We add the first log block for the selected job
+      logs.push(logsLines[currentLineIndex]);
+
+      do {
+        // We add log block lines until to find a new log block (next line to start by a date)
+        if (logsLines.length > currentLineIndex + 1) {
+          const maybeDate = logsLines[currentLineIndex + 1].substring(0, 19);
+          isAValidDate = dayjs(maybeDate).isValid();
+          if (!isAValidDate) {
+            logs.push(logsLines[currentLineIndex + 1]);
+            currentLineIndex++;
+          }
+        }
+      } while (!isAValidDate && logsLines.length > currentLineIndex + 1);
+    }
+
+    if (logsLines && logsLines.length > currentLineIndex + 1) {
+      // We go to the next line to find a new log block for the selected job
+      return [
+        ...logs,
+        ...getLogsForAJob(logsLines, currentLineIndex + 1, jobIdString),
+      ];
+    }
+  }
+
+  return logs;
+};
 
 /**
  * Logic to start the external job instance.
@@ -21,11 +63,7 @@ exports.start = async ({ job, instance }) => {
       }
     );
 
-    console.log({ result });
-
     const { data } = result;
-
-    console.log({ data });
 
     // You can return any payload you want to get in the stop and getStatus functions.
     return Response.success(data);
@@ -80,12 +118,61 @@ exports.getStatus = async ({ job, instance }) => {
 exports.getLogs = async ({ job, instance }) => {
   try {
     console.log('GET LOG INSTANCE:', instance);
-    const { data } = await axios.get(
-      `${job.featuresValues.endpoint.url}/api/demo/datasets/${job.featuresValues.dataset.id}/logs`,
+    const result = await axios.get(
+      `${job.featuresValues.endpoint.url}/rest/v4/admin/logs`,
+      {
+        auth: {
+          username: job.featuresValues.endpoint.username,
+          password: job.featuresValues.endpoint.password
+        },
+        responseType: 'arraybuffer'
+      }
     );
 
-    return Response.success(data.logs.map((item) => Log(item.log, item.output, item.time)));
+    const jobLogsFilePath = '/tmp/knime-logs.zip';
+    const jobLogsFolderPath = '/tmp/knime-logs';
+
+    const { data } = result;
+
+    if (fs.existsSync(jobLogsFilePath)) {
+      fs.unlinkSync(jobLogsFilePath);
+    }
+
+    fs.appendFileSync(jobLogsFilePath, data);
+
+    if (fs.existsSync(jobLogsFolderPath)) {
+      rimraf.sync(jobLogsFolderPath);
+    }
+
+    await extract(jobLogsFilePath, { dir: jobLogsFolderPath });
+
+    const directories = fs.readdirSync(jobLogsFolderPath, { withFileTypes: true })
+      .filter(dirent => dirent.isDirectory())
+      .map(dirent => dirent.name);
+
+    const jobIdString = `: ${job.featuresValues.job.id} :`;
+
+    console.log({ jobIdString });
+
+    let jobLogs = [];
+
+    directories.forEach((dir) => {
+      if (fs.existsSync(`${jobLogsFolderPath}/${dir}/knime.log`)) {
+
+        // We are getting all lines in new log file
+        const newLogs = fs.readFileSync(`${jobLogsFolderPath}/${dir}/knime.log`, 'utf8');
+
+        const logsLines = newLogs.split('\n');
+
+        if (logsLines && logsLines.length > 0) {
+          jobLogs = jobLogs.concat(getLogsForAJob(logsLines, 0, jobIdString));
+        }
+      }
+    });
+
+    return Response.success(jobLogs.map((jobLogLine) => Log(jobLogLine, Stream.STDOUT, null)));
   } catch (error) {
-    return Response.error(`Failed to get log for dataset ${job.featuresValues.dataset.id}`, { error });
+    console.log({ error });
+    return Response.error('Failed to get logs', { error });
   }
 };
