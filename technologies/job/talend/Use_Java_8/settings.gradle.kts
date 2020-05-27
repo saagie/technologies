638b71ab8560a9bfb09c8edf6f4252cff0afc9f3
Yann PETIT <yann.petit@saagie.com>
2020-05-27 15:14:16 +0200

    [talend] corrects gradle project name

--- technologies/job/talend/Use_Java_8/settings.gradle.kts
+++ technologies/job/talend/Use_Java_8/settings.gradle.kts
@@ -15,4 +15,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-rootProject.name = "Use_Java_8"
+rootProject.name = "talend-Use_Java_8"
