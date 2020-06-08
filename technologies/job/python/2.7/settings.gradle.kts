ff86fe56ff61ad4c21dd4c5c6d8b49bcb3d30e99
Yann PETIT <yann.petit@saagie.com>
2020-06-08 17:39:37 +0200

    [python] corrects typo

--- technologies/job/python/2.7/settings.gradle.kts
+++ technologies/job/python/2.7/settings.gradle.kts
@@ -15,4 +15,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-rootProject.name = "python2.7-with-libs"
+rootProject.name = "python2.7"
