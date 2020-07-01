475333f0cdb1afef588e49ac713a8f957ab66961
Yann PETIT <yann.petit@saagie.com>
2020-07-01 09:16:55 +0200

    [python] remove useless test.sh script and add RealeasNotes for Python technologies

--- technologies/job/python/python-3.6/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-docker run --rm -i -v $PWD:/python3 -v /var/run/docker.sock:/var/run/docker.sock saagie/container-structure-test structure-test  test --image $1 --config python3/image_test.yml
