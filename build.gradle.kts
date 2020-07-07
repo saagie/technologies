9c033c3346019be939e74a744c1fc7a6243a95c2
Yann PETIT <yann.petit@saagie.com>
2020-07-07 16:17:46 +0200

    [spark] use plugin technology sdk 1.2.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ buildscript {
         mavenLocal()
     }
     dependencies {
-        classpath("com.saagie:technologiesplugin:1.2.7")
+        classpath("com.saagie:technologiesplugin:1.2.8")
     }
 }
 apply<SaagieTechnologiesPackageGradlePlugin>()
