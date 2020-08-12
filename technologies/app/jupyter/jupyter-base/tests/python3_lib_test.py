64e21f66ce53a8c5a4f6b63a1c00a8137c165e32
Yann PETIT <public@ypetit.net>
2020-08-12 12:09:45 +0200

    [SDKTECHNO-56] refacto, comments ...

--- technologies/app/jupyter/jupyter-base/tests/python3_lib_test.py
+++ technologies/app/jupyter/jupyter-base/tests/python3_lib_test.py
@@ -1,12 +1,9 @@
 # Manual tests ...
-
 import sys
 print(sys.executable)
 print(sys.version)
 print(sys.version_info)
 
-###
-### Test conda install
 ### FIXME find a way to test those installs
-#from hdfs.hfile import Hfile
-#import hdf5
+# - from hdfs.hfile import Hfile
+# - import hdf5
