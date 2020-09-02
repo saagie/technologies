e7b2910064a7ea0cacb41c377f11d2a3afdff757
Nicolas Torion <nicolas.torion@bearstudio.fr>
2020-09-02 16:27:25 +0200

    [gcp-cloud-data-transfer] Add transfer job stop action and options on run

--- technologies/job/gcp-cloud-data-transfer/transfer-jobs/instanceActions.js
+++ technologies/job/gcp-cloud-data-transfer/transfer-jobs/instanceActions.js
@@ -1,12 +1,12 @@
 const { Response, JobStatus } = require('@saagie/sdk');
-const dayjs = require('dayjs');
+const moment = require('moment-timezone');
 const { google } = require('googleapis');
 const storagetransfer = google.storagetransfer('v1');
 const { getAuth, getErrorMessage } = require('../utils');
 const { JOB_STATUS } = require('../job-states');
 
 /**
- * Logic to start a new job.
+ * Logic to start a new GCP Cloud Data Transfer job.
  * @param {Object} params
  * @param {Object} params.job - Contains job data including featuresValues.
  */
@@ -16,40 +16,58 @@ exports.start = async ({ job }) => {
 
     const auth = getAuth(gcpKey);
 
-    const currentDate = dayjs();
+    const currentDate = moment();
+
+    const runObject = {
+      projectId : job.featuresValues.project.id,
+      description: job.featuresValues.jobName,
+      status: 'ENABLED',
+      schedule: {
+        scheduleStartDate: {
+          day: currentDate.day(),
+          month: currentDate.month(),
+          year: currentDate.year()
+        },
+      },
+      transferSpec: {
+        gcsDataSource: {
+          bucketName: job.featuresValues.sourceBucket.id,
+        },
+        gcsDataSink: {
+          bucketName: job.featuresValues.destinationBucket.id,
+        },
+        transferOptions: {
+          deleteObjectsFromSourceAfterTransfer: false,
+          deleteObjectsUniqueInSink: false,
+          overwriteObjectsAlreadyExistingInSink: false
+        }
+      }
+    };
+
+    if (job.featuresValues.dailyExecutionHour && job.featuresValues.dailyExecutionHour.length > 0) {
+      const dailyHour = moment(job.featuresValues.dailyExecutionHour, 'HH:mm');
+
+      const dailyHourUTC = dailyHour.utc();
+
+      if (dailyHourUTC) {
+        runObject.schedule.startTimeOfDay = {
+          hours: dailyHourUTC.hour(),
+          minutes: dailyHourUTC.minute(),
+          seconds: 0,
+          nanos: 0,
+        };
+      }
+    } else {
+      runObject.schedule.scheduleEndDate = {
+        day: currentDate.day(),
+        month: currentDate.month(),
+        year: currentDate.year()
+      };
+    }
 
     const { data } = await storagetransfer.transferJobs.create({
       auth,
-      requestBody: {
-        projectId : job.featuresValues.project.id,
-        description: job.featuresValues.jobName,
-        status: 'ENABLED',
-        schedule: {
-          scheduleStartDate: {
-            day: currentDate.day(),
-            month: currentDate.month(),
-            year: currentDate.year()
-          },
-          scheduleEndDate: {
-            day: currentDate.day(),
-            month: currentDate.month(),
-            year: currentDate.year()
-          }
-        },
-        transferSpec: {
-          gcsDataSource: {
-            bucketName: job.featuresValues.sourceBucket.id,
-          },
-          gcsDataSink: {
-            bucketName: job.featuresValues.destinationBucket.id,
-          },
-          transferOptions: {
-            deleteObjectsFromSourceAfterTransfer: false,
-            deleteObjectsUniqueInSink: false,
-            overwriteObjectsAlreadyExistingInSink: false
-          }
-        }
-      }
+      requestBody: runObject
     });
 
     return Response.success({ data });
@@ -59,16 +77,51 @@ exports.start = async ({ job }) => {
 };
 
 /**
- * Logic to stop the job
+ * Logic to stop the GCP Cloud Data Transfer job
  * @param {Object} params
  * @param {Object} params.job - Contains job data including featuresValues.
  * @param {Object} params.instance - Contains instance data including the payload returned in the start function.
  */
-exports.stop = async () => {
+exports.stop = async ({ job, instance }) => {
+  try {
+    const gcpKey = JSON.parse(job.featuresValues.endpoint.jsonKey);
+
+    const auth = getAuth(gcpKey);
+
+    const filterOptions = {
+      project_id: job.featuresValues.project.id,
+      job_names: [instance.payload.data.name]
+    };
+  
+    const filterOptionsString = JSON.stringify(filterOptions);
+  
+    const { data: { operations } } = await storagetransfer.transferOperations.list({
+      auth,
+      name: 'transferOperations',
+      filter: filterOptionsString
+    });
+
+    let promises = [];
+  
+    if (operations && operations.length > 0) {
+      promises = operations.map(async (operation) => {
+        await storagetransfer.transferOperations.cancel({
+          auth,
+          name: operation.name
+        });
+      });
+    }
+
+    await Promise.all(promises);
+
+    return Response.success();
+  } catch (error) {
+    return getErrorMessage(error, 'Failed to stop GCP Cloud Data Transfer job');
+  }
 };
 
 /**
- * Logic to retrieve the Dataflow job status.
+ * Logic to retrieve the GCP Cloud Data Transfer job status.
  * @param {Object} params
  * @param {Object} params.job - Contains job data including featuresValues.
  * @param {Object} params.instance - Contains instance data including the payload returned in the start function.
@@ -100,15 +153,8 @@ exports.getStatus = async ({ job, instance }) => {
 
     return Response.success(JobStatus.AWAITING);
   } catch (error) {
-    return getErrorMessage(error, 'Failed to get status for GCP Dataflow job ');
+    return getErrorMessage(error, 'Failed to get status for GCP Cloud Data Transfer job ');
   }
 };
 
-/**
- * Logic to retrieve the Dataflow job logs.
- * @param {Object} params
- * @param {Object} params.job - Contains job data including featuresValues.
- * @param {Object} params.instance - Contains instance data including the payload returned in the start function.
- */
-exports.getLogs = async () => {
-};
+exports.getLogs = () => {};
