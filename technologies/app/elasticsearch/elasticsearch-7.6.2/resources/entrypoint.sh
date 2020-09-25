bbbe83e2211b476ad196c3a8434f90f74cd086fe
Yann PETIT <public@ypetit.net>
2020-09-25 16:27:35 +0200

    [SDKTECHNO-73][appelasticsearch] use nginx, refacto, test build with buildkit

--- technologies/app/elasticsearch/elasticsearch-7.6.2/resources/entrypoint.sh
+++ technologies/app/elasticsearch/elasticsearch-7.6.2/resources/entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-sed -i 's:SAAGIE_BASE_PATH:'"$SAAGIE_BASE_PATH"':g' /etc/nginx/sites-enabled/elasticsearch.conf
-nginx && /usr/local/bin/docker-entrypoint.sh
+if [ -z "$SAAGIE_BASE_PATH" ]
+then
+      SAAGIE_BASE_PATH=/
+fi
+echo ""
+echo "*** Setting Saagie's custom Base Path to [$SAAGIE_BASE_PATH]"
+sed -i 's:SAAGIE_BASE_PATH:'"$SAAGIE_BASE_PATH"':g' /etc/nginx/nginx.conf
+echo ""
+echo "Starting nginx reverse proxy"
+nginx
+echo ""
+echo "Starting elasticsearch [${@}]"
+/usr/local/bin/docker-entrypoint.sh
