704be544a64a09db2273743e1dab49b2af20468a
Yann PETIT <public@ypetit.net>
2020-09-27 16:35:37 +0200

    [SDKTECHNO-73][appelasticsearch] fix issue on exposed ports

--- technologies/app/elasticsearch/elasticsearch-7.6.2/resources/entrypoint.sh
+++ technologies/app/elasticsearch/elasticsearch-7.6.2/resources/entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 if [ -z "$SAAGIE_BASE_PATH" ]
 then
-      SAAGIE_BASE_PATH=/
+      SAAGIE_BASE_PATH=/9200/
+fi
+
+if [ -z "$SAAGIE_BASE2_PATH" ]
+then
+      SAAGIE_BASE2_PATH=/9300/
 fi
 echo ""
-echo "*** Setting Saagie's custom Base Path to [$SAAGIE_BASE_PATH]"
+echo "*** Setting Saagie's custom Base Path to [$SAAGIE_BASE_PATH}"
 sed -i 's:SAAGIE_BASE_PATH:'"$SAAGIE_BASE_PATH"':g' /etc/nginx/nginx.conf
+echo "*** Setting Saagie's custom Base2 Path to [$SAAGIE_BASE2_PATH}"
+sed -i 's:SAAGIE_BASE2_PATH:'"$SAAGIE_BASE2_PATH"':g' /etc/nginx/nginx.conf
 echo ""
 echo "Starting nginx reverse proxy ..."
 nginx
