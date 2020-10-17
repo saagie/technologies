5b02b7be879c16ac0cd98a654017d2a7da5545e4
Yann PETIT <public@ypetit.net>
2020-10-17 09:50:53 +0200

    [SDKTECHNO-72][apptoucantoco] WIP - docker-compose in docker OK - one cumulative Dockerfile front/back/redis/mongo OK - toucan needs customized parameters

--- technologies/app/toucantoco/toucantoco-74.2/resources/entrypoint.sh
+++ technologies/app/toucantoco/toucantoco-74.2/resources/entrypoint.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-sed -i 's:80:'88':g' /etc/nginx/conf.d/default.conf
+set -xe
+
+sed -i 's:80:88:g' /etc/nginx/conf.d/front.conf
+sed -i 's:^bind:#bind:g' /etc/redis/redis.conf
+
+# Trick to update /etc/hosts inside a container to avoid '/etc/hosts': Device or resource busy
+cp /etc/hosts /etc/hosts_tmp
+sed -i 's:127\.0\.0\.1.*:127\.0\.0\.1 localhost redis mongo:g' /etc/hosts_tmp
+cp /etc/hosts_tmp /etc/hosts
+rm /etc/hosts_tmp
+
+
+/etc/init.d/redis-server start
+mongod --dbpath /var/lib/mongodb --logpath /var/log/mongod/mongod.log --bind_ip_all --fork
+
+netstat -antlp
