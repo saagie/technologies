5795480b284f65332e42f35b6bf2b00a444eed30
Yann PETIT <public@ypetit.net>
2020-10-23 23:01:17 +0200

    [SDKTECHNO-72][apptoucantoco] clean code

--- technologies/app/toucantoco/ReleaseNotes.md
+++ technologies/app/toucantoco/ReleaseNotes.md
@@ -4,6 +4,10 @@ ReleaseNotes
 Changes by date
 ---------------
 
+### 2020-10-23
+
+ - First working Toucantoco release
+
 ### 2020-09-22
 
  - Firsttry to add ToucanToc to technologies repository
--- technologies/app/toucantoco/toucan-compose/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker run --rm -it --name toucan-compose \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -e SAAGIE_BASE_PATH_FRONT=/front \
-  -e SAAGIE_BASE_PATH_BACK=/back \
-  -e DOCKER_USER=finalspy \
-  -e DOCKER_PASSWD=$DOCKER_PASSWD \
-  $1
