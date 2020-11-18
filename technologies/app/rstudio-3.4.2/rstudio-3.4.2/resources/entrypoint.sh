c0745e0d25e64c54654be4890e1ef8970b807091
Yann PETIT <public@ypetit.net>
2020-11-18 23:18:03 +0100

    [SDKTECHNO-85] try to add error if admin pass is not set, and override default PASSWORD variable name to something less generic

--- technologies/app/rstudio-3.4.2/rstudio-3.4.2/resources/entrypoint.sh
+++ technologies/app/rstudio-3.4.2/rstudio-3.4.2/resources/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+if [ -z ${RSTUDIO_ADMIN_PASSWD+x} ]; then echo "Environment variable RSTUDIO_ADMIN_PASSWD is not set !"; echo "Please set it !"; echo " => exiting"; fi
+if [ -z ${RSTUDIO_PASSWD+x} ]; then echo "Environment variable RSTUDIO_PASSWD is not set !"; echo "Please set it !"; echo " => exiting";else export PASSWORD=`echo $RSTUDIO_PASSWD`; fi
+
 sed -i 's:SAAGIE_BASE_PATH:'"$SAAGIE_BASE_PATH"':g' /etc/nginx/sites-enabled/rstudio.conf
 nginx && /init_rstudio.sh
