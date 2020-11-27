a668f19335f7ccd975740c47f8ad6b2513727514
Yann PETIT <public@ypetit.net>
2020-11-27 15:55:37 +0100

    [apporientdb] add dockerhub documentation, corrects pathes

--- technologies/app/orientdb/orientdb/resources/dockerd-entrypoint.sh
+++ technologies/app/orientdb/orientdb/resources/dockerd-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo ""
-echo "*** Setting Saagie's custom Base Path to [$TS_PATH}"
+echo "*** Setting Saagie's custom Base Path to [$TS_PATH]"
 sed -i 's:REPLACE_TS_PATH:'"$TS_PATH"':g' /etc/nginx/nginx.conf
 
 echo ""
-echo "*** Setting Saagie's custom Base Path to [$TS2_PATH}"
+echo "*** Setting Saagie's custom Base Path to [$TS2_PATH]"
 sed -i 's:REPLACE_TS2_PATH:'"$TS2_PATH"':g' /etc/nginx/nginx.conf
 
 # OPTION 2 replace in place the redirect
