7638c419b09e47e04fdc95f460e327dd84e63376
Thomas Quiviger <tquiviger@gmail.com>
2021-10-22 09:52:22 +0200

    Adding Jupyerlab

---
+++ technologies/app/jupyterlab/jupyterlab-base/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+chown -R jovyan /notebooks-dir
+
+start-notebook.sh --KernelSpecManager.ensure_native_kernel=False --ServerApp.token='' --ServerApp.password='' --ServerApp.base_url=$SAAGIE_BASE_PATH
