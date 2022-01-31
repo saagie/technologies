5ccc10b84226ff6cc14b23e1d805b6484ac5c9cb
Augustin Peyridieux <augustin.peyridieux@saagie.com>
2022-01-31 11:49:40 +0100

    update Dash and mlFlow entrypoint

--- technologies/app/dash/dash/resources/entrypoint.sh
+++ technologies/app/dash/dash/resources/entrypoint.sh
@@ -2,6 +2,23 @@
 
 set -xeuo pipefail
 
+echo "
+- INFO: 'DASH_GIT_URL_REPOSITORY' variable is mandatory and should be set to the name of the git repository containning your Dash app source code (e.g.:https://github.com/saagie/demo-r-shiny.git).
+- INFO: 'DASH_GIT_BRANCH' is optional and defaulted to master, it indicates a specific branch to checkout.
+
+- INFO: the default branch for gitlab is 'master'
+- INFO: the default branch for github is 'main'
+
+INFO: If you hae any request don't hesitate to create an issue in this repository: https://github.com/saagie/technologies
+INFO: Did you take a look to our community repository? https://github.com/saagie/technologies-community
+"
+
+if [[ -z "${SHINY_GIT_URL_REPOSITORY}" ]]; then
+  echo "ERROR : Variable DASH_GIT_URL_REPOSITORY must be set before running the app. 
+  See https://docs.saagie.io/user/latest/tutorials/projects-module/projects/envar/index.html#projects-create-envar-project for more information"
+  exit 1
+fi
+
 DASH_GIT_BRANCH=${DASH_GIT_BRANCH:-master}
 echo "Setting DASH_GIT_BRANCH to $DASH_GIT_BRANCH..."
 
--- technologies/app/mlflow-server-1.16/mlflow-server-1.16/resources/entrypoint.sh
+++ technologies/app/mlflow-server-1.16/mlflow-server-1.16/resources/entrypoint.sh
@@ -2,18 +2,39 @@
 
 set -xeo pipefail
 
-if [[ -z "${MLFLOW_BACKEND_STORE_URI}" ]]; then
-  echo "ERROR : MLFLOW_BACKEND_STORE_URI environment variable must be set"
-  echo "Usage : MLFLOW_BACKEND_STORE_URI = database-backed store as SQLAlchemy database URI <dialect>+<driver>://<username>:<password>@<host>:<port>/<database> MLflow supports the database dialects mysql, mssql, sqlite, and postgresql."
+echo """
+INFO : 'MLFLOW_BACKEND_STORE_URI' variable is mandatory and must be set as SQLAlchemy database URI:
+INFO : <dialect>+<driver>://<username>:<password>@<host>:<port>/<database> (MLflow supports the database dialects mysql, mssql, sqlite, and postgresql)
+INFO : example: mysql://login:password@192.168.56.30:3306/db_name
+INFO : Notice: The Database must be created before launching MlFlow
+
+INFO : MLFLOW_DEFAULT_ARTIFACTORY_ROOT:  variable is mandatory and must be set to an HDFS directory (default directory: e.g. hdfs://cluster:8020/artifactory/mlflow )
+INFO : Notice: The HDFS directory must be created before launching MlFlow
+
+INFO : If you hae any request don't hesitate to create an issue in this gitlab: https://github.com/saagie/technologies
+INFO : Did you take a look to our community repository? https://github.com/saagie/technologies-community
+"""
+
+if [[ -z ${MLFLOW_BACKEND_STORE_URI} || -z ${MLFLOW_DEFAULT_ARTIFACTORY_ROOT} ]]; then
+  echo
+  """
+  ERROR : Missing environment variable. In order to work, this app needs the following environment variables set : 
+  MLFLOW_BACKEND_STORE_URI and MLFLOW_DEFAULT_ARTIFACTORY_ROOT
+
+  Usage : 'MLFLOW_BACKEND_STORE_URI' variable is mandatory and must be set as SQLAlchemy database URI:
+  Usage : <dialect>+<driver>://<username>:<password>@<host>:<port>/<database> (MLflow supports the database dialects mysql, mssql, sqlite, and postgresql)
+  Usage : example: mysql://login:password@192.168.56.30:3306/db_name
+  Usage : Notice: The Database must be created before launching MlFlow
+
+  INFO : MLFLOW_DEFAULT_ARTIFACTORY_ROOT:  variable is mandatory and must be set to an HDFS directory (default directory: e.g. hdfs://cluster:8020/artifactory/mlflow )
+  Usage : The HDFS directory must be created before launching MlFlow
+  """
   exit 1
-elif [[ -z "${MLFLOW_DEFAULT_ARTIFACTORY_ROOT}" ]]; then
-  echo "ERROR : MLFLOW_DEFAULT_ARTIFACTORY_ROOT environment variable must be set"
-  echo "Usage : MLFLOW_DEFAULT_ARTIFACTORY_ROOT = default location to server’s artifact store (e.g. hdfs://cluster:8020/artifactory/mlflow )"
-  exit 1
-else
-  echo "Starting MLflow Server"
-  mlflow server \
-    --backend-store-uri ${MLFLOW_BACKEND_STORE_URI} \
-    --default-artifact-root ${MLFLOW_DEFAULT_ARTIFACTORY_ROOT} \
-    --host 0.0.0.0
 fi
+
+
+echo "Starting MLflow Server"
+mlflow server \
+  --backend-store-uri ${MLFLOW_BACKEND_STORE_URI} \
+  --default-artifact-root ${MLFLOW_DEFAULT_ARTIFACTORY_ROOT} \
+  --host 0.0.0.0
