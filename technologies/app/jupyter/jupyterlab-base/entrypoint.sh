eee1e0bb850eeefb51ef6b59b970ab296a673c18
Thomas Quiviger <8511444+tquiviger@users.noreply.github.com>
2022-04-07 15:03:08 +0200

    Sdktechno 169 - Python Hadoop classpath (#430)
    * SDKTECHNO-169 :  CLASSPATH for Hadoop is set at runtime and without widlcards
    
    * Updating Hadoop classpath for JupyterLab as well

--- technologies/app/jupyter/jupyterlab-base/entrypoint.sh
+++ technologies/app/jupyter/jupyterlab-base/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export CLASSPATH=`hadoop classpath --glob`
+
 chown -R jovyan /notebooks-dir
 
 start-notebook.sh --KernelSpecManager.ensure_native_kernel=False --ServerApp.token='' --ServerApp.password='' --ServerApp.base_url=$SAAGIE_BASE_PATH
