82ec8113cb01f58d1e368ac3954d1fd45cca773e
Tony <tony@saagie.com>
2022-05-19 15:37:54 +0200

    fix: only build R images

---
+++ technologies/job/r/r-4.2/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euo pipefail
+
+R_HOME=$(Rscript -e 'Sys.getenv("R_HOME")' | sed -rn 's/^\[[[:digit:]]+\] "(.*)"/\1/p')
+
+# Change default CRAN to ENV VAR if present
+if [[ -z "${R_CUSTOM_CRAN}" ]]; then
+    echo "No custom CRAN defined, using default, you can configure one using R_CUSTOM_CRAN env var"
+else
+    echo "Custom CRAN used: ${R_CUSTOM_CRAN}"
+    sed -ri "s#(CRAN = ')[^']*#\1${R_CUSTOM_CRAN}#g" "${R_HOME}/etc/Rprofile.site"
+fi
+
+if compgen -G "*.zip" > /dev/null; then
+    echo "Zip file detected ... unzipping ..."
+    unzip -qo *.zip
+elif compgen -G "*.tar.gz" > /dev/null; then
+    echo "Tar gz file detected ... unzipping ..."
+    tar -xf *.tar.gz
+elif compgen -G "*.tar" > /dev/null; then
+    echo "Tar file detected ... unzipping ..."
+    tar -xf *.tar
+fi
+
+if test -f main_script; then
+    echo "'main_script' detected ... executing ..."
+    bash ./main_script
+else
+    exec "$@"
+fi
