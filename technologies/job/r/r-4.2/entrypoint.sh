c31dcfb34d30d0eef5da04dc8413dca08ad4b902
Tony <tony@saagie.com>
2022-07-12 15:38:41 +0200

    fix typo

--- technologies/job/r/r-4.2/entrypoint.sh
+++ technologies/job/r/r-4.2/entrypoint.sh
@@ -6,7 +6,7 @@ R_HOME=$(Rscript -e 'Sys.getenv("R_HOME")' | sed -rn 's/^\[[[:digit:]]+\] "(.*)"
 
 # Change default CRAN to ENV VAR if present
 if [[ "${R_CUSTOM_CRAN:-empty}" == "empty" ]]; then
-    echo "No custom CRAN defined, using default, you can configure one using R_CUSTOM_CRAN env var"
+    echo "No custom CRAN defined, using default, you can configure one using R_CUSTOM_CRAN env var."
 else
     echo "Custom CRAN used: ${R_CUSTOM_CRAN}"
     sed -ri "s#(CRAN = ')[^']*#\1${R_CUSTOM_CRAN}#g" "${R_HOME}/etc/Rprofile.site"
