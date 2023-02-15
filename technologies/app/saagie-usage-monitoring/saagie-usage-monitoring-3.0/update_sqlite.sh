0f39989b90998eb2062f38ff26ebbf8a05b1331c
NicolasCD <nicolas@creativedata.fr>
2023-02-15 15:16:48 +0100

    Sdktechno 218 - Problem with comlplex password (#539)
    * add single quote around passwords
    
    * change time update database grafana

--- technologies/app/saagie-usage-monitoring/saagie-usage-monitoring-3.0/update_sqlite.sh
+++ technologies/app/saagie-usage-monitoring/saagie-usage-monitoring-3.0/update_sqlite.sh
@@ -1,3 +1,3 @@
-sleep 20
+sleep 30
 echo "Changing alerts from 'provisionned' to 'standard'"
 sqlite3 /opt/grafana/grafana.db "UPDATE provenance_type SET provenance = '' WHERE record_type = 'alertRule'"
