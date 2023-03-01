422e550224d4358bf94fc8486341a9fa9466a2f6
Nicolas <nicolas@saagie.com>
2023-03-01 16:09:42 +0100

    add provisionning alerts gestion

--- technologies/app/saagie-usage-monitoring/saagie-usage-monitoring-3.0/update_sqlite.sh
+++ technologies/app/saagie-usage-monitoring/saagie-usage-monitoring-3.0/update_sqlite.sh
@@ -1,3 +1,11 @@
+echo "Remove provisionned alerts if already exists"
+
+if [[ -f "/opt/grafana/grafana.db" ]]; then
+    echo "Removing /etc/grafana/provisioning/alerting"
+    rm -R /etc/grafana/provisioning/alerting
+fi
+
+
 sleep 30
 echo "Changing alerts from 'provisionned' to 'standard'"
 sqlite3 /opt/grafana/grafana.db "UPDATE provenance_type SET provenance = '' WHERE record_type = 'alertRule'"
