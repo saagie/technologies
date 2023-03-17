7a10e559c74a9c6d7dd3246e4d485e6cc237ddf8
NicolasCD <nicolas@creativedata.fr>
2023-03-17 14:50:25 +0100

    Sdktechno 200 (#554)
    add dbt

---
+++ technologies/job/dbt/dbt-1.4/resources/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if compgen -G "*.zip*" > /dev/null;
+then
+    unzip -q *.zip
+else
+    echo "ERROR : You must submit a zip file containing your dbt project"
+fi
+
+if test -f main_script;
+then sh ./main_script;
+else exec "$@"
+fi;
