8b338097d78606d822b5dd33ef0e098e056128c7
Qiwei <qiwei.he@saagie.com>
2023-05-30 12:11:52 +0200

    fix nginx version, change log level to warning

--- technologies/app/airbyte/airbyte-1.0/code/__main__.py
+++ technologies/app/airbyte/airbyte-1.0/code/__main__.py
@@ -78,7 +78,7 @@ def main():
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
+    logging.basicConfig(level=logging.WARNING,
                         format="%(asctime)s [%(levelname)s] %(message)s",
                         datefmt="%d/%m/%Y %H:%M:%S")
     main()
