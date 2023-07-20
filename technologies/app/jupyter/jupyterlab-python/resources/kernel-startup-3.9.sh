c579b586aa05fa9d8c1cfca4757dea5cea346723
Qiwei <qiwei.he@saagie.com>
2023-07-20 16:39:23 +0200

    remove jupyter python part

--- technologies/app/jupyter/jupyterlab-python/resources/kernel-startup-3.9.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# activate anaconda env
-source activate py39
-
-# this is the critical part, and should be at the end of your script:
-exec /opt/conda/envs/py39/bin/python -m ipykernel_launcher $@
