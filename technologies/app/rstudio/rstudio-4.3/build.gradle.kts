6171fa59f0da7c102d29b3c6b9d8876988aad8c9
Nicolas <nicolas@saagie.com>
2023-08-09 14:24:17 +0200

    force build

--- technologies/app/rstudio/rstudio-4.3/build.gradle.kts
+++ technologies/app/rstudio/rstudio-4.3/build.gradle.kts
@@ -26,6 +26,6 @@ apply<SaagieTechnologiesGradlePlugin>()
 tasks.withType(com.bmuschko.gradle.docker.tasks.image.DockerBuildImage::class) {
      this.buildArgs.put(
         "base_img",
-        "saagie/r-image:4.3-1.163.0_sdktechno-238"
+        "saagie/r-image:4.3-1.164.0_SDKTECHNO-239"
     )
 }
