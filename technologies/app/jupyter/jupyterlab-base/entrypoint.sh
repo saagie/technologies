0f30c9c63c99e8fb899dbd42deb4266d9cbd9679
Qiwei <qiwei.he@saagie.com>
2023-08-29 15:44:15 +0200

    rename jupyterlab

--- technologies/app/jupyter/jupyterlab-base/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export CLASSPATH=`hadoop classpath --glob`
-
-chown -R jovyan /notebooks-dir
-
-start-notebook.sh --KernelSpecManager.ensure_native_kernel=False --ServerApp.token='' --ServerApp.password='' --ServerApp.base_url=$SAAGIE_BASE_PATH
--- technologies/app/jupyter/jupyterlab/resources/kernel-startup-3.10.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# activate anaconda env
-source activate py310
-
-# this is the critical part, and should be at the end of your script:
-exec /opt/conda/envs/py310/bin/python -m ipykernel_launcher $@
