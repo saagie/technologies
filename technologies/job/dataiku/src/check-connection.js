5132043c058dd32420aa584abdccd8b5301e6f0f
tnducrocq <tnducrocq@gmail.com>
2024-08-09 15:21:01 +0200

    SDKTECHNO-265 remove external techno (#646)
    * SDKTECHNO-265 remove external techno
    
    * [BOT] Set version for this branch
    
    ---------
    
    Co-authored-by: Tony Ducrocq <tony.ducrocq@saagie.com>
    Co-authored-by: GitHub Action Bot <github-action@saagie.com>

--- technologies/job/dataiku/src/check-connection.js
+++
@@ -1,9 +0,0 @@
-import {buildClient} from './client';
-
-exports.checkConnection = async ({connection}) => {
-    const client = await buildClient(connection);
-    await client.getProjects();
-    return {
-        ok: true
-    };
-};
--- technologies/scripts/aws/src/check-connection.js
+++
@@ -1,9 +0,0 @@
-import {buildClient} from './client';
-
-exports.checkConnection = async ({connection}) => {
-    const client = buildClient(connection);
-    await client.sts.getCallerIdentity();
-    return {
-        ok: true
-    };
-};
