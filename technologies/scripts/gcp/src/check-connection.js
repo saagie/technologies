7a3e2304b5c33ca1ad48484a330794e6325c9cad
tnducrocq <tnducrocq@gmail.com>
2024-08-09 15:21:01 +0200

    SDKTECHNO-265 remove external techno (#646)
    * SDKTECHNO-265 remove external techno
    
    * [BOT] Set version for this branch
    
    ---------
    
    Co-authored-by: Tony Ducrocq <tony.ducrocq@saagie.com>
    Co-authored-by: GitHub Action Bot <github-action@saagie.com>

--- technologies/scripts/gcp/src/check-connection.js
+++
@@ -1,9 +0,0 @@
-import {buildClient} from './client';
-
-exports.checkConnection = async ({connection}) => {
-    const client = await buildClient(connection);
-    await client.cloudresourcemanager.projects.list();
-    return {
-        ok: true
-    };
-};
