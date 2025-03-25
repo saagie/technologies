4a5c7dfbddadb5194d70424a539c15d234990986
Julien Bignon <julien.bignon@saagie.com>
2025-03-25 20:46:18 +0100

    download zip only if url

--- technologies/job/spark/spark-2.4/innerContexts/python/entrypoint.sh
+++ technologies/job/spark/spark-2.4/innerContexts/python/entrypoint.sh
@@ -44,40 +44,19 @@ if [ -n "$SPARK_EXTRA_CLASSPATH" ]; then
   SPARK_CLASSPATH="$SPARK_CLASSPATH:$SPARK_EXTRA_CLASSPATH"
 fi
 
+# BEGIN SAAGIE SPECIFIC CODE
 cd /sandbox
- # parse content and if pyfiles extract minio url and inject it
-if [ -f main_script ] && grep -q "\--py-files" main_script;
+if [ -f *.zip ]
 then
-  PYSPARK_FILES="`grep -Po '.*--py-files=\K[^ ]+' main_script`"
-fi;
-
-if [ -n "$PYSPARK_FILES" ]; then
-    PYTHONPATH="$PYTHONPATH:$PYSPARK_FILES"
-    #Copy and unzip pyfiles
-    if [[ $PYSPARK_FILES == *[,]* ]];then
-      echo "PYSPARK_FILES contains comma"
-      pyfiles=$(echo $PYSPARK_FILES | tr "," "\n")
-
-      for file in $pyfiles
-      do
-          echo ">>> [$file]"
-          wget -nv $file
-      done
-    else
-      echo ">>> [$PYSPARK_FILES]"
-      wget -nv $PYSPARK_FILES
-    fi
-    if [ -f *.zip ]
-    then
-      unzip -q *.zip
-    fi
-    if [ -f "requirements.txt" ]
-    then
-      pip install -r requirements.txt
-    fi
-    rm -Rf /opt/spark/work-dir
-    ln -s /sandbox/ /opt/spark/work-dir
+  unzip -q *.zip
 fi
+if [ -f "requirements.txt" ]
+then
+  pip install -r requirements.txt
+fi
+rm -Rf /opt/spark/work-dir
+ln -s /sandbox/ /opt/spark/work-dir
+# END SAAGIE SPECIFIC CODE
 
 PYSPARK_ARGS=""
 if [ -n "$PYSPARK_APP_ARGS" ]; then
